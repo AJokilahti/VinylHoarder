64dd230050ac5351f469e65c1069cefd75e68836
Aleksi Jokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-03-30 17:18:00 +0300

    Adding components Login.js for logging in Home.js for user homescreen

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '.components/Home.js';
+import LoginScreen from '.components/Login.js';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  
+
+
   return (
     <View style={styles.container}>
       <Text>VinylHoarder</Text>
---
+++ components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Login(){
+    // Add login screen implementing Firebase auth
+
+    return (
+        <View>Login screen</View>
+    )
+}
