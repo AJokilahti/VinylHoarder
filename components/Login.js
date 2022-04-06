501568ce332b378a93ff9a4658c2299caacd3c28
Aleksi Jokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-04-06 11:34:01 +0300

    Added firebase.js and code to Login.js in Login.js: added inputs and buttons for login and register in firebase.js: added firebaseConfig

--- components/Login.js
+++ components/Login.js
@@ -1,26 +1,112 @@
-import React, {useState} from 'react';
-import { View, Text, Button, TextInput, Image } from 'react-native';
-import HomeScreen from './Home.js'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from'@react-navigation/native-stack';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  Image,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
+import HomeScreen from "./Home.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { withTheme } from "react-native-elements";
 
 const Stack = createNativeStackNavigator();
 
-export default function Login({navigation}){
-    // Add login screen implementing Firebase auth
+export default function Login({ navigation }) {
+  // Add login screen implementing Firebase auth
 
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-    
-    return (
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-        <View>
-        <TextInput label></TextInput>
-        
-        <Button 
-          title="Sign in"
-          onPress={() => navigation.navigate('Home')}
-          />
-        </View>
-    )
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={userEmail}
+          onChangeText={text=> setUserEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={userPassword}
+          onChangeText={text=> setUserPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => {  }} style={styles.loginButton}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {  }}
+          style={[styles.registerButton, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    width: '80%',
+    
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop:40,
+  },
+  loginButton: {
+    backgroundColor: "green",
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  registerButton: {
+    backgroundColor: "blue",
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center"
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight:'700',
+    fontSize: 16,
+  },
+  
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight:'700',
+    fontSize: 16,
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB44gLJ6NkSRuGEi9EgLlwrP3UGScXhRhI",
+
+  authDomain: "fir-auth-2a472.firebaseapp.com",
+
+  projectId: "fir-auth-2a472",
+
+  storageBucket: "fir-auth-2a472.appspot.com",
+
+  messagingSenderId: "208482812301",
+
+  appId: "1:208482812301:web:157b269c6958e6c9423dc4",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
